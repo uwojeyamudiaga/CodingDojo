e8563e1adb88b43d352a04d1a04fe8b6e1ceab1d
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-15 20:08:18 +0100

    Add boilerplate code to uwojeyamudiaga.js

---
+++ challenges/2022-03-15-longest-common-prefix/solutions/javascript/uwojeyamudiaga/uwojeyamudiaga.js
@@ -0,0 +1,10 @@
+// # Longest Common Prefix
+// 
+// Given a list of strings, return a string containing the longest prefix in
+// common amongst all the provided strings. 
+
+function longestCommonPrefix(strings) {
+    //Type your solution here
+  }
+  
+  module.exports = { longestCommonPrefix };
