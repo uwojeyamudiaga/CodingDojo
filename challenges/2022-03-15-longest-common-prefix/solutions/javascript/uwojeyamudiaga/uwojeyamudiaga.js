76dd2bd893138c8ca6e48d14f7da7743c0b4350d
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-16 19:31:57 +0100

    Add counter, pointer, length, result and firstString variables

--- challenges/2022-03-15-longest-common-prefix/solutions/javascript/uwojeyamudiaga/uwojeyamudiaga.js
+++ challenges/2022-03-15-longest-common-prefix/solutions/javascript/uwojeyamudiaga/uwojeyamudiaga.js
@@ -6,6 +6,12 @@
 function longestCommonPrefix(strings) {
     //Type your solution here
     if (!strings.length || !Array.isArray(strings)) return "";
+
+    let pointer = 0, counter = 1, length = strings.length, result = "";
+    let firstString = strings[pointer];
+
+   
+
   }
   
   module.exports = { longestCommonPrefix };
